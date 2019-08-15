f5a6bb44d98e7b5f504077628f978298d8152995
Levi-Huynh <Levi_huynh@yahoo.com>
2019-08-15 11:07:24 -0700

    user authentication and authorization routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      Hello World
-    </div>
-  );
-}
+import Navigation from './components/Navigation/index.jsx';
+import Landing from './views/Landing';
+import SignUpPage from './components/Auth/SignUp.jsx';
+import SignInPage from './components/Auth/SignIn.jsx';
+import Home from './views/Home';
 
-export default App;
+import * as ROUTES from './Routes/routes';
+import { withAuthentication } from './components/Session';
+
+
+const App = () => (
+  <Router>
+    <Navigation />
+    <Route exact path={ROUTES.LANDING} component={Landing} />
+    <Route exact path={ROUTES.SIGN_UP} component={SignUpPage} />
+    <Route exact path={ROUTES.SIGN_IN} component={SignInPage} />
+    <Route exact path={ROUTES.HOME} component={Home} />
+  </Router>
+);
+
+export default withAuthentication(App);
