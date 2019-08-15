15db76f69e5fb4c077e45f987e80e9c4728d77d3
Memitaru <amandatwah@gmail.com>
2019-08-14 20:31:01 -0700

    Adding axios and first get request
    Co-authored-by: David Aceves <digital0david@gmail.com>
    Co-authored-by: Diamond Purvis <Purvisdiamond@gmail.com>

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,24 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+
+  const [data, setData] = useState({message: ""})
+
+  useEffect(() => {
+    const loadData = async () => {
+      const result = await axios.get('https://wheretocode-master.herokuapp.com/');
+      setData(result.data);
+    }
+    loadData();
+  })
+
+
   return (
+    
     <div className="App">
       Hello World
+      {data.message}
     </div>
   );
 }
