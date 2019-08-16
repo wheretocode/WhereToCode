f0dbbcc970ef707e8e6d6dc7325c4dd35cf0a53a
Ronny Alvarado <rsalvarado777@gmail.com>
2019-08-15 22:44:16 -0500

    Set up MongoDB configuration and connection
    Co-authored-by: Ronny Alvarado <Rsalvarado777@gmail.com>
    Co-authored-by: Reed Turgeon <rturgeon13@gmail.com>
    Co-authored-by: Bernard Johnson <bj2336@email.vccs.edu>

--- backend/config/connectDB.js
+++ backend/config/connectDB.js
@@ -1,19 +1,18 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const mongoURI = process.env.mongoURI;
 
 const connectDB = async () => {
-    try {
-        await mongoose.connect(mongoURI, {
-            useNewUrlParser: true,
-            useCreateIndex: true,
-            useFindAndModify: false
-        });
-        console.log('MongoDB connected...');
-    } catch(err) {
-        console.log(err);
-    }
-}
+  try {
+    await mongoose.connect(mongoURI, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false
+    });
+    console.log("MongoDB connected...");
+  } catch (err) {
+    console.log(err);
+  }
+};
 
 module.exports = connectDB;
-
--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 //ENV Configuration
-require('dotenv').config();
+require("dotenv").config();
 
 // SERVER
 const server = require("./api/server.js");
-const connectDB = require('./config/connectDB');
+const connectDB = require("./config/connectDB");
 
 // PORT
 const PORT = process.env.PORT || 9001;
 
 // Connect to MongoDB
+// Must whitelist IP address for those connecting to the database
 connectDB();
 
 // LISTENING
