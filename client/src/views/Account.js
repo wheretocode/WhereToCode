2260cb8bae5ed50a700814a864883c25ef49f4ac
Levi-Huynh <Levi_huynh@yahoo.com>
2019-08-17 13:40:07 -0700

    added authorization check to accounts view page

--- client/src/views/Account.js
+++ client/src/views/Account.js
@@ -1,14 +1,21 @@
 import React from 'react';
 
+import { AuthUserContext, withAuthorization } from '../components/Session';
 import { PasswordForgetForm } from '../components/Auth/PasswordForget.jsx';
 import PasswordChangeForm from '../components/Auth/PasswordChange.jsx';
 
 const AccountPage = () => (
-    <div>
-        <h1>Account Page</h1>
-        <PasswordForgetForm />
-        <PasswordChangeForm />
-    </div>
+    <AuthUserContext.Consumer>
+        {authUser => (
+            <div>
+                <h1>Account Page</h1>
+                <h1>Account: {authUser.email}</h1>
+                <PasswordForgetForm />
+                <PasswordChangeForm />
+            </div>
+        )}
+    </AuthUserContext.Consumer>
 );
 
-export default AccountPage;
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(AccountPage);
