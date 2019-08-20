e590d684ad63b7d8ee2e086dab396c8cf940813f
Trip1701 <bj2336@email.vccs.edu>
2019-08-19 23:35:39 -0400

    Network Dasplay Keys More Readable for Users

--- client/src/components/NetworkDashboard.jsx
+++ client/src/components/NetworkDashboard.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 
 const NetworkDashboard = () => {
+    //Obtain Available Network Information from User's Browser
     const networkInterface = navigator.connection 
                              || navigator.mozConnection 
                              || navigator.webkitConnection;
 
+    //Creates an Array of Arrays of the Network Data for Mapping
     const network = Object.entries({
         type: networkInterface.type,
         effectiveType: networkInterface.effectiveType,
@@ -15,7 +17,15 @@ const NetworkDashboard = () => {
         onchange: networkInterface.onchange
     });
 
-    console.log(network);
+    //Network Information Key to Display for Users
+    const networkKey = {
+        type: 'Connection Type',
+        effectiveType: 'Effective Connection Type',
+        downlink: 'Downlink Speed',
+        downlinkMax: 'Max Downlink Speed',
+        rtt: 'Round Trip Time',
+        saveData: 'Reduced Data Usage',
+    }
 
     return(
         <div>
@@ -24,13 +34,16 @@ const NetworkDashboard = () => {
             <div>
                 {
                     network.map(data => {
+                        //Check for Available Data Out of Possible Options
                         if(data[1] !== null && data[1] !== undefined) {
 
                             return <p key={Math.random()}>
-                                        {`${data[0]}: ${data[1]}`}
+                                        {`${networkKey[data[0]]}: ${data[1]}`}
                                     </p>
 
                         }
+                        //Removes Warning for No Return on Arrow Function
+                        return null;
                     })
                 }
             </div>
