dea5314d847ce90331f83343ffd86a7fb629670d
Trip1701 <bj2336@email.vccs.edu>
2019-08-22 23:36:18 -0400

    App and Navigation Test Suites Built

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,9 +1,32 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import App from './App';
+import Firebase, { FirebaseContext } from './Firebase/index';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+import { cleanup, render } from '@testing-library/react';
+
+beforeEach(cleanup);
+afterEach(cleanup);
+
+describe("<App />", () => {
+  it('should render without crashing', () => {
+    render( <FirebaseContext.Provider value={new Firebase()}>
+              <App />
+            </FirebaseContext.Provider> );
+  });
+
+  it('should render a navigation bar', () => {
+    const { getByText } = render( <FirebaseContext.Provider value={new Firebase()}>
+                                    <App />
+                                  </FirebaseContext.Provider> );
+
+    getByText(/sign in/i);
+  });
+
+  it('should render Landing Page by Default', () => {
+    const { getByText } = render( <FirebaseContext.Provider value={new Firebase()}>
+                                    <App />
+                                  </FirebaseContext.Provider> );
+
+    getByText(/landing/i);
+  });
 });
