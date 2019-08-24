6ff6aa28eee8ff5d84370f6a81b85ec6ab543c10
Levi-Huynh <Levi_huynh@yahoo.com>
2019-08-23 20:32:26 -0700

    add jest.mock to fix failed firebase tests

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -1,32 +1,48 @@
 import React from 'react';
 import App from './App';
 import Firebase, { FirebaseContext } from './Firebase/index';
+import * as firebase from 'firebase/app'
 
 import { cleanup, render } from '@testing-library/react';
 
 beforeEach(cleanup);
 afterEach(cleanup);
 
+jest.mock('axios')
+jest.mock('firebase/app')
+jest.mock('firebase/auth')
+
+
 describe("<App />", () => {
+  afterEach(cleanup);
+
+  it('renders with firebase auth', async () => {
+    firebase.auth = jest.fn().mockReturnValue({
+      onAuthStateChanged: cb => () => cb()
+    })
+  })
+
   it('should render without crashing', () => {
-    render( <FirebaseContext.Provider value={new Firebase()}>
-              <App />
-            </FirebaseContext.Provider> );
+    render(<FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>);
   });
+
 
   it('should render a navigation bar', () => {
-    const { getByText } = render( <FirebaseContext.Provider value={new Firebase()}>
-                                    <App />
-                                  </FirebaseContext.Provider> );
+    const { getByText } = render(<FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>);
 
     getByText(/sign in/i);
-  });
+  })
 
   it('should render Landing Page by Default', () => {
-    const { getByText } = render( <FirebaseContext.Provider value={new Firebase()}>
-                                    <App />
-                                  </FirebaseContext.Provider> );
+    const { getByText } = render(<FirebaseContext.Provider value={new Firebase()}>
+      <App />
+    </FirebaseContext.Provider>);
 
     getByText(/landing/i);
   });
+
 });
