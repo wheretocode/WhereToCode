48a6267c2a255f0decdb0b45bb76a68cf02b0ea8
Trip1701 <bj2336@email.vccs.edu>
2019-08-26 23:32:14 -0400

    NetworkDashboard Component Refactor for Use With New API

--- client/src/components/NetworkDashboard.jsx
+++ client/src/components/NetworkDashboard.jsx
@@ -1,70 +1,30 @@
 import React from 'react';
+import axios from 'axios';
 
-const NetworkDashboard = () => {
-    //Obtain Available Network Information from User's Browser
-    const networkInterface = navigator.connection 
-                             || navigator.mozConnection 
-                             || navigator.webkitConnection;
+class NetworkDashboard extends React.Component {
+    constructor(props) {
+        super(props);
 
-    //Creates an Array of Arrays of the Network Data for Mapping
-    let network = [];
-
-    if(networkInterface !== undefined) {
-        network = Object.entries({
-            type: networkInterface.type,
-            effectiveType: networkInterface.effectiveType,
-            downlink: networkInterface.downlink,
-            downlinkMax: networkInterface.downlinkMax,
-            rtt: networkInterface.rtt,
-            saveData: networkInterface.saveData,
-            onchange: networkInterface.onchange
-        });
+        this.state = {}
     }
 
-    //Network Information Key to Display for Users
-    const networkKey = {
-        type: 'Connection Type',
-        effectiveType: 'Effective Connection Type',
-        downlink: 'Downlink Speed',
-        downlinkMax: 'Max Downlink Speed',
-        rtt: 'Round Trip Time',
-        saveData: 'Reduced Data Usage',
-    }
-
-    return(
-        <div>
-            <h2>Network Dashboard</h2>
-
-            <div data-testid='info-box' >
-                {
-                    network.map(data => {
-                        //Check for Available Data Out of Possible Options
-                        //data[0] = key, data[1] = value
-                        if(data[1] !== null && data[1] !== undefined) {
-                            
-                            //Check If Time Units Need To Be Added
-                            let displayData;
-
-                            if(data[0] === 'downlink' || data[0] === 'downlinkMax') {
-                                displayData = `${data[1]} Mb/s`;
-                            } else if(data[0] === 'rtt') {
-                                displayData = `${data[1]} ms`
-                            } else {
-                                displayData = data[1];
-                            }
-
-                            return <p key={Math.random()}>
-                                        {`${networkKey[data[0]]}: ${displayData}`}
-                                    </p>
-
-                        }
-                        //Removes Warning for No Return on Arrow Function
-                        return null;
+    componentDidMount() {
+        axios.get('http://localhost:9001/api/network')
+             .then(res => {
+                 console.log(res.body)
+                        this.setState({ ...res.body })
                     })
-                }
+             .catch(err => console.log(err));
+    }
+
+    render() {
+        return(
+            <div>
+                Hell World
             </div>
-        </div>
-    );
+        );
+    }
+
 }
 
 export default NetworkDashboard;
