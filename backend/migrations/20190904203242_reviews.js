af93d5cb2e375186425311b0f02ac3b0b86884f1
Memitaru <amandatwah@gmail.com>
2019-09-12 20:30:38 -0700

    Changes to review migrations

--- backend/migrations/20190904203242_reviews.js
+++ backend/migrations/20190904203242_reviews.js
@@ -3,6 +3,10 @@ exports.up = async function(knex) {
     tbl.increments(); // primary key - reviews id
     tbl.integer("rating").notNullable(); // simple rating system 1-3 (may change)
     tbl.string("comments").notNullable(); // review comments
+    tbl.integer("internet_rating").notNullable();
+    tbl.decimal("upload_speed");
+    tbl.decimal("download_speed")
+    tbl.boolean("secure_wifi").defaultTo(false);
     tbl
       .integer("user_id") // foreign key - user id
       .references("id")
