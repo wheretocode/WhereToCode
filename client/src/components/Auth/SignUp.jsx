e0228288da92421f7872174f56895224e90b00bc
Ronny Alvarado <rsalvarado777@gmail.com>
2019-09-12 23:27:07 -0500

    Included post request signup for our backend in addition to firebase backend. Refactored code into React Hooks. May have merge conflicts with regards to SignOut form for some reason. Code still work in progress due to not able to render, but no errors in console

--- client/src/components/Auth/SignUp.jsx
+++ client/src/components/Auth/SignUp.jsx
@@ -1,113 +1,104 @@
-import React, { Component } from 'react';
-import { Link, withRouter } from 'react-router-dom';
+import React, { useState } from "react";
+import { Link, withRouter } from "react-router-dom";
 
-import { withFirebase } from '../../Firebase';
-import * as ROUTES from '../../Routes/routes';
+import { withFirebase } from "../../Firebase";
+import * as ROUTES from "../../Routes/routes";
 
-import { Form, FormField, Button, Box, Text, Heading } from 'grommet';
+import axios from "axios";
+
+import { Form, FormField, Button, Box, Text, Heading } from "grommet";
 
 const SignUpPage = () => (
-    <Box align="center" background="#555555" height="100vh" pad="large">
-        <Heading level="2" responsive="true" size="medium" alignSelf="center">Sign Up</Heading>
-        <SignUpForm />
-    </Box>
+  <Box align="center" background="#555555" height="100vh" pad="large">
+    <Heading level="2" responsive="true" size="medium" alignSelf="center">
+      Sign Up
+    </Heading>
+    <SignUpForm />
+  </Box>
 );
 
-const INITIAL_STATE = {
-    username: '',
-    email: '',
-    passwordOne: '',
-    passwordTwo: '',
-    error: null,
+const SignUpFormBase = props => {
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [passwordOne, setPasswordOne] = useState("");
+  const [passwordTwo, setPasswordTwo] = useState("");
+  const [error, setError] = useState(null);
+
+  const onSubmit = event => {
+    props.firebase
+      .doCreateUserWithEmailAndPassword(email, passwordOne)
+      .then(user => {
+        const newUser = {
+          firebase_user_id: user.uid,
+          userName: username,
+          email: email
+        };
+        axios
+          .get("http://wheretocode-master.herokuapp.com/auth/register", newUser)
+          .then(res => {
+            console.log(res.data);
+            setUsername("");
+            setEmail("");
+            setPasswordOne("");
+            props.history.push(ROUTES.HOME);
+          })
+          .catch(error => {
+            console.log(error);
+          });
+      })
+      .catch(error => {
+        setError(error);
+      });
+    event.preventDefault();
+  };
+
+  const isInvalid =
+    passwordOne !== passwordTwo ||
+    passwordOne === "" ||
+    email === "" ||
+    username === "";
+
+  return (
+    <Box width="medium">
+      <Form onSubmit={onSubmit}>
+        <FormField
+          name="username"
+          value={username}
+          onChange={e => setUsername(e.target.value)}
+          type="text"
+          placeholder="Full Name"
+        />
+        <FormField
+          name="email"
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          type="text"
+          placeholder="Email Address"
+        />
+        <FormField
+          name="passwordOne"
+          value={passwordOne}
+          onChange={e => setPasswordOne(e.target.value)}
+          type="password"
+          placeholder="Password"
+        />
+        <FormField
+          name="passwordTwo"
+          value={passwordTwo}
+          onChange={e => setPasswordTwo(e.target.value)}
+          type="password"
+          placeholder="Confirm Password"
+        />
+        <Button disabled={isInvalid} type="submit" primary label="Sign Up" />
+      </Form>
+    </Box>
+  );
 };
 
-class SignUpFormBase extends Component {
-    constructor(props) {
-        super(props);
-
-        this.state = { ...INITIAL_STATE };
-    }
-
-    onSubmit = event => {
-        const { username, email, passwordOne } = this.state;
-
-        this.props.firebase
-            .doCreateUserWithEmailAndPassword(email, passwordOne)
-            .then(authUser => {
-                this.setState({ ...INITIAL_STATE });
-                this.props.history.push(ROUTES.HOME);
-            })
-            .catch(error => {
-                this.setState({ error });
-            });
-
-        event.preventDefault();
-    };
-
-    onChange = event => {
-        this.setState({ [event.target.name]: event.target.value });
-    };
-
-    render() {
-        const {
-            username,
-            email,
-            passwordOne,
-            passwordTwo,
-            error,
-        } = this.state;
-
-        const isInvalid =
-            passwordOne !== passwordTwo ||
-            passwordOne === '' ||
-            email === '' ||
-            username === '';
-
-        return (
-            <Box width="medium">
-            <Form onSubmit={this.onSubmit}>
-                <FormField
-                    name="username"
-                    value={username}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Full Name"
-                />
-                <FormField
-                    name="email"
-                    value={email}
-                    onChange={this.onChange}
-                    type="text"
-                    placeholder="Email Address"
-                />
-                <FormField
-                    name="passwordOne"
-                    value={passwordOne}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Password"
-                />
-                <FormField
-                    name="passwordTwo"
-                    value={passwordTwo}
-                    onChange={this.onChange}
-                    type="password"
-                    placeholder="Confirm Password"
-                />
-                <Button disabled={isInvalid} type="submit" primary label="Sign Up" />
-                   
-
-                {error && <p>{error.message}</p>}
-            </Form>
-            </Box>
-        );
-    }
-}
-
 const SignUpLink = () => (
-    <Text alignSelf="center" margin="small">
-        Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-    </Text>
+  <Text alignSelf="center" margin="small">
+    Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
+  </Text>
 );
 const SignUpForm = withRouter(withFirebase(SignUpFormBase));
 export default SignUpPage;
