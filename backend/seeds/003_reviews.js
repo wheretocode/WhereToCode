ff78cdb786908ece52ad7a21994c707d921ea75a
Memitaru <amandatwah@gmail.com>
2019-09-15 14:55:03 -0700

    Updated reviews seeding

--- backend/seeds/003_reviews.js
+++ backend/seeds/003_reviews.js
@@ -8,24 +8,40 @@ exports.seed = function(knex) {
         {
           rating: 2,
           comments: "Mediocre chicken.",
+          internet_rating: 2,
+          upload_speed: 2.51,
+          download_speed: 3,
+          secure_wifi: false,
           user_id: 1,
           location_id: 1
         },
         {
           rating: 3,
           comments: "Best chicken I've ever had!",
+          internet_rating: 3,
+          upload_speed: 5.2,
+          download_speed: 8.59,
+          secure_wifi: false,
           user_id: 1,
-          location_id: 1
+          location_id: 2
         },
         {
           rating: 1,
           comments: "HORRIBLE CHICKEN",
+          internet_rating: 1,
+          upload_speed: 2,
+          download_speed: 3.5,
+          secure_wifi: false,
           user_id: 3,
           location_id: 2
         },
         {
           rating: 2,
           comments: "Good Rooster",
+          internet_rating: 2,
+          upload_speed: 4.2,
+          download_speed: 10.3,
+          secure_wifi: true,
           user_id: 2,
           location_id: 3
         }
