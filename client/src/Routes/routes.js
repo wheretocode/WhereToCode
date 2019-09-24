7d0b48490782e5e70e6ded37bc8c87d8bf14b2e0
Diamond Purvis <purvisdiamond@gmail.com>
2019-09-23 20:47:40 -0400

    Added routes - review - signout

--- client/src/Routes/routes.js
+++ client/src/Routes/routes.js
@@ -1,6 +1,8 @@
 export const SIGN_UP = '/signup';
 export const SIGN_IN = '/signin';
+export const SIGN_OUT_BUTTON ='/';
 export const HOME = '/home';
 export const LANDING = '/';
 export const ACCOUNT = '/account';
 export const PASSWORD_FORGET = '/pw-forget';
+export const REVIEW = '/review';
