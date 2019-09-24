c954a4f3b2d220e5d54ecddc9da0530971d9ef3f
Diamond Purvis <purvisdiamond@gmail.com>
2019-09-23 20:47:03 -0400

    Added tests - second navigation

--- client/src/components/Navigation/Navigation.test.js
+++ client/src/components/Navigation/Navigation.test.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Navigation from './index.jsx';
+import Navigation2 from './index.jsx';
 
 import { BrowserRouter as Router } from 'react-router-dom';
 
@@ -31,4 +32,30 @@ describe('<Navigation />', () => {
 
     getByText(/sign up/i);
   });
+});
+
+describe('<Navigation2 />', () => {
+  it('should render without crashing', () => {
+      render( <Router>
+                  <Navigation2 />
+              </Router> );
+  });
+
+  
+it('should render a sign in button', () => {
+  const { getByText } = render( <Router>
+                                  <Navigation2 />
+                                </Router> );
+
+  getByText(/sign in/i);
+});
+
+
+it('should render a sign up button', () => {
+  const { getByText } = render( <Router>
+                                  <Navigation2 />
+                                </Router> );
+
+  getByText(/sign up/i);
+});
 });
