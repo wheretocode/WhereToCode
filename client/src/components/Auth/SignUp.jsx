7b36486e4c6e3921a2ee97464ee1b300d4044c9d
Levi-Huynh <Levi_huynh@yahoo.com>
2019-09-30 15:27:43 -0700

    sign up and login modal

--- client/src/components/Auth/SignUp.jsx
+++ client/src/components/Auth/SignUp.jsx
@@ -4,18 +4,135 @@ import { Link, withRouter } from "react-router-dom";
 import { withFirebase } from "../../Firebase";
 import * as ROUTES from "../../Routes/routes";
 
-import axios from "axios";
+import styled from "styled-components";
 
+import axios from "axios";
 import { Form, FormField, Button, Box, Text, Heading } from "grommet";
 
-const SignUpPage = () => (
-    <Box align="center" background="#555555" height="100vh" pad="large">
-        <Heading level="2" responsive="true" size="medium" alignSelf="center">
-            Sign Up
-    </Heading>
-        <SignUpForm />
-    </Box>
-);
+import Modal, { ModalProvider, BaseModalBackground } from "styled-react-modal";
+
+// const SignUpPage = () => (
+//     <Box align="center" background="#555555" height="100vh" pad="large">
+//         <Heading level="2" responsive="true" size="medium" alignSelf="center">
+//             Sign Up
+//     </Heading>
+//         <SignUpForm />
+//     </Box>
+// );
+
+
+const StyledModal = Modal.styled`
+  width: 20rem;
+  height: 20rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  opacity: ${props => props.opacity};
+  transition: opacity ease 500ms;
+`;
+
+const FormContainer = styled.div`
+display: flex;
+flex-direction: column;
+justify-content: center;
+align-items: center;
+text-align: center;
+width: 100%
+height: 100%;
+border-radius: 25px;
+background:white;
+border: 3px solid gold;
+
+`;
+
+
+const StyledHeader = styled.div`
+width: 100%;
+height: 100px;
+display: flex;
+flex-direction: row;
+justify-content: center;
+align-items: baseline;
+background:black;
+color: white;
+position: relative;
+margin-top: -70px;
+border-radius: 30px;
+border: 3px solid gold;
+border-bottom: none;
+`;
+
+const StyledForm = styled.form`
+display:flex;
+flex-direction: column;
+justify-content: center;
+align-items: center
+text-align: center;
+margin-top: 30px;
+background: white;
+width: 70%;
+`;
+
+const StyledSvg = styled.svg`
+position: absolute;
+  bottom: 0;
+  width: 100%;
+  height: 50px;
+
+`;
+
+const StyledInput = styled.input`
+opacity: .5;
+// border-radius: 25px;
+border: none;
+border-bottom: .7px solid grey;
+color: grey;
+padding-left: 10px;
+margin-left: 10px;
+margin-top: 15px;
+margin-bottom: 15px;
+font-size: 18px;
+font-family: "Poppins", serif;
+text-align: left;
+height: 30px;
+background: none;
+// box-shadow: 0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19);
+::placeholder: gold;
+width: 70%;
+`;
+
+// const SignUpButton = styled.button`
+// width: 70%;
+// border-radius: 10px;
+// background: black;
+// border: 1px solid gold;
+// color: white;
+// height: 10%;
+// text-align: center;
+// margin-top: 8%;
+// font-family: "Zilla Slab", serif;
+// font-size: 1.5rem;
+
+// `;
+
+const SignUpButton = styled.button`
+width: 55%;
+border-radius: 10px;
+background: black;
+border: 3px solid gold;
+color: white;
+height: 10%;
+text-align: center;
+margin-top: 8%;
+font-family: "Zilla Slab", serif;
+font-size: 1.5rem;
+display: flex;
+justify-content: center;
+align-items: center;
+
+`;
+
 
 const SignUpFormBase = props => {
     //Hooks to update state 
@@ -65,48 +182,66 @@ const SignUpFormBase = props => {
         email === "" ||
         username === "";
 
+
+
     return (
-        <Box width="medium">
-            <Form onSubmit={onSubmit}>
-                <FormField
+
+
+        <FormContainer>
+            <StyledHeader>
+                <i class="fas fa-wifi fa-2x" style={{ color: "gold", marginRight: "14px" }}></i>
+                <h1 >HiveStack</h1>
+                <StyledSvg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 200 100" preserveAspectRatio="none">
+                    <circle fill="white" cx="0" cy="100" r="100" />
+                    <circle fill="white" cx="200" cy="100" r="100" />
+                </StyledSvg>
+            </StyledHeader>
+            <StyledForm onSubmit={onSubmit}>
+                <StyledInput
                     name="username"
                     value={username}
                     onChange={e => setUsername(e.target.value)}
                     type="text"
-                    placeholder="Full Name"
+                    placeholder="Username"
                 />
-                <FormField
+                <StyledInput
                     name="email"
                     value={email}
                     onChange={e => setEmail(e.target.value)}
                     type="text"
-                    placeholder="Email Address"
+                    placeholder="Email"
                 />
-                <FormField
+                <StyledInput
                     name="passwordOne"
                     value={passwordOne}
                     onChange={e => setPasswordOne(e.target.value)}
                     type="password"
                     placeholder="Password"
+
                 />
-                <FormField
+                <StyledInput
                     name="passwordTwo"
                     value={passwordTwo}
                     onChange={e => setPasswordTwo(e.target.value)}
                     type="password"
                     placeholder="Confirm Password"
                 />
-                <Button disabled={isInvalid} type="submit" primary label="Sign Up" />
-            </Form>
-        </Box>
+
+            </StyledForm>
+            <SignUpButton disabled={isInvalid} onClick={onSubmit} primary label="Sign Up" >Sign Up</SignUpButton>
+
+            {/* <button onClick={props.toggleModal}>Close</button> */}
+        </FormContainer >
+
+
     );
 };
 
 const SignUpLink = () => (
-    <Text alignSelf="center" margin="small">
+    <h6 alignSelf="center" margin="small">
         Don't have an account? <Link to={ROUTES.SIGN_UP}>Sign Up</Link>
-    </Text>
+    </h6>
 );
 const SignUpForm = withRouter(withFirebase(SignUpFormBase));
-export default SignUpPage;
+// export default SignUpPage;
 export { SignUpForm, SignUpLink };
--- client/src/components/Navigation/index.jsx
+++ client/src/components/Navigation/index.jsx
@@ -1,5 +1,5 @@
-import React from "react";
-
+import React, { useState } from "react";
+import Modal, { ModalProvider, BaseModalBackground } from "styled-react-modal";
 import SignOutButton from "../Auth/SignOut.jsx";
 import * as ROUTES from "../../Routes/routes";
 
@@ -9,7 +9,156 @@ import { Box, Button, RoutedButton, Heading, Anchor } from "grommet";
 
 import { Link } from "react-router-dom";
 
+
+import { SignUpForm } from "../Auth/SignUp.jsx";
+import { SignInForm } from "../Auth/SignIn.jsx";
+
 import styled from "styled-components";
+
+
+//styled modal is css for pop up
+const StyledModal = Modal.styled`
+  width: 30rem;
+  height: 30rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: white;
+  opacity: ${props => props.opacity};
+  transition: opacity ease 1000ms;
+  border-radius: 30px;
+`;
+
+// const StyledModal = Modal.styled`
+//   width: 20rem;
+//   height: 20rem;
+//   display: flex;
+//   align-items: center;
+//   justify-content: center;
+//   background-color: white;
+//   opacity: ${props => props.opacity};
+//   transition: opacity ease 500ms;
+// `;
+
+const RegisterLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  border: 1px solid gold;
+  background-color: gold;
+  border-radius: 5px;
+  font-size: 1.5rem;
+  padding: 5px 20px;
+  font-family: "Zilla Slab", serif;
+  margin-right: 20px;
+  &:hover {
+    background-color: yellow;
+  }
+`;
+
+const Navbar = styled.div`
+  position: absolute;
+  display: flex;
+  justify-content: space-between;
+  width: 100%;
+  padding-top: 20px;
+`;
+
+
+
+const LoginLink = styled(Link)`
+  text-decoration: none;
+  color: white;
+  margin-right: 10px;
+  font-size: 1.5rem;
+  padding: 5px 20px;
+  font-family: "Zilla Slab", serif;
+  border-radius: 5px;
+`;
+
+
+function FancyModalButton() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [opacity, setOpacity] = useState(0);
+
+  function toggleModal(e) {
+    setIsOpen(!isOpen);
+  }
+
+  function afterOpen() {
+    setTimeout(() => {
+      setOpacity(1);
+    }, 20);
+  }
+
+  function beforeClose() {
+    return new Promise(resolve => {
+      setOpacity(0);
+      setTimeout(resolve, 200);
+    });
+  }
+
+
+  return (
+    <div>
+      <RegisterLink onClick={toggleModal}>Sign Up</RegisterLink>
+      <StyledModal
+        isOpen={isOpen}
+        afterOpen={afterOpen}
+        beforeClose={beforeClose}
+        onBackgroundClick={toggleModal}
+        onEscapeKeydown={toggleModal}
+        opacity={opacity}
+        backgroundProps={{ opacity }}
+      >
+        <SignUpForm toggleModal={toggleModal} />
+
+        {/* <button onClick={toggleModal}>Close me</button> */}
+      </StyledModal>
+    </div>
+  );
+}
+
+function LoginButton() {
+  const [isOpen, setIsOpen] = useState(false);
+  const [opacity, setOpacity] = useState(0);
+
+  function toggleModal(e) {
+    setIsOpen(!isOpen);
+  }
+
+  function afterOpen() {
+    setTimeout(() => {
+      setOpacity(1);
+    }, 20);
+  }
+
+  function beforeClose() {
+    return new Promise(resolve => {
+      setOpacity(0);
+      setTimeout(resolve, 200);
+    });
+  }
+
+
+  return (
+    <div>
+      <LoginLink onClick={toggleModal}>Login</LoginLink>
+      <StyledModal
+        isOpen={isOpen}
+        afterOpen={afterOpen}
+        beforeClose={beforeClose}
+        onBackgroundClick={toggleModal}
+        onEscapeKeydown={toggleModal}
+        opacity={opacity}
+        backgroundProps={{ opacity }}
+      >
+        <SignInForm toggleModal={toggleModal} />
+
+        {/* <button onClick={toggleModal}>Close me</button> */}
+      </StyledModal>
+    </div>
+  );
+}
 
 const Navigation = () => (
   <Navbar>
@@ -31,46 +180,27 @@ const NavigationAuth = () => (
   </Box>
 );
 
+
+const FadingBackground = styled(BaseModalBackground)`
+opacity: ${props => props.opacity};
+transition: opacity ease 1000ms;
+`;
+
 const NavigationNonAuth = () => (
+
   <Box direction="row" justify="right" gap="small">
-    <ul>
-      <LoginLink to={ROUTES.SIGN_IN}>Login</LoginLink>
-      <RegisterLink to={ROUTES.SIGN_UP}>Sign Up</RegisterLink>
-    </ul>
+    {/* 
+    <LoginLink to={ROUTES.SIGN_IN}>Login</LoginLink> */}
+    <ModalProvider backgroundComponent={FadingBackground}>
+      <LoginButton />
+    </ModalProvider>
+    <ModalProvider backgroundComponent={FadingBackground}>
+      <FancyModalButton />
+      {/* <FancyModalButton to={ROUTES.SIGN_UP}/> */}
+    </ModalProvider>
+
   </Box>
 );
 
 export default Navigation;
 
-const Navbar = styled.div`
-  position: absolute;
-  display: flex;
-  justify-content: space-between;
-  width: 100%;
-  padding-top: 20px;
-`;
-
-const RegisterLink = styled(Link)`
-  text-decoration: none;
-  color: black;
-  border: 1px solid gold;
-  background-color: gold;
-  border-radius: 5px;
-  font-size: 1.5rem;
-  padding: 5px 20px;
-  font-family: "Zilla Slab", serif;
-  margin-right: 20px;
-  &:hover {
-    background-color: yellow;
-  }
-`;
-
-const LoginLink = styled(Link)`
-  text-decoration: none;
-  color: white;
-  margin-right: 10px;
-  font-size: 1.5rem;
-  padding: 5px 20px;
-  font-family: "Zilla Slab", serif;
-  border-radius: 5px;
-`;
