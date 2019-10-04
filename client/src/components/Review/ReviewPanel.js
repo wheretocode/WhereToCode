3fb3e29c4fd3abedad90370fed14986e875d3664
Diamond Purvis <purvisdiamond@gmail.com>
2019-10-03 20:39:33 -0400

    Basic tab modal layout

---
+++ client/src/components/Review/ReviewPanel.js
@@ -0,0 +1,96 @@
+import React from "react";
+import Popup from "reactjs-popup";
+import styled from "styled-components";
+import Rating from './Rating';
+import Modal, { ModalProvider, BaseModalBackground } from "styled-react-modal";
+
+
+const Button = styled.button`
+  /* Adapt the colors based on primary prop */
+  background: ${props => props.primary ? "blue" : "white"};
+  color: ${props => props.primary ? "white" : "blue"};
+
+  font-size: 1em;
+  margin: 1em;
+  padding: 0.25em 1em;
+  border: 2px solid blue;
+  border-radius: 3px;
+`
+
+const StyleModal = styled.div`
+    font-size: 12px;
+  `
+
+  const Header = styled.div`
+    width: 100%;
+    border-bottom: 1px solid gray;
+    font-size: 18px;
+    text-align: center;
+    padding: 5px;
+ `
+  const Content = styled.div`
+  
+    width: 100%;
+    padding: 10px 5px;
+  `
+  const Actions = styled.div`
+    width: 100%;
+    padding: 10px 5px;
+    margin: auto;
+    text-align: center;
+  `
+  const Close = styled.div`
+    cursor: pointer;
+    position: absolute;
+    display: block;
+    padding: 2px 5px;
+    line-height: 20px;
+    right: -10px;
+    top: -10px;
+    font-size: 24px;
+    background: #ffffff;
+    border-radius: 18px;
+    border: 1px solid #cfcece;
+  `
+
+export default ({ close }) => (
+    <StyleModal>
+    <Header> Leave A Review </Header>
+    <Content>      {" "}
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
+      Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
+      delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
+      <br />
+      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
+      commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
+      explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
+      </Content>    
+      <Actions>
+      <Popup
+        trigger={<Button> Trigger </Button>}
+        position="top center"
+        closeOnDocumentClick
+      >
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+          magni omnis delectus nemo, maxime molestiae dolorem numquam
+          mollitia, voluptate ea, accusamus excepturi deleniti ratione
+          sapiente! Laudantium, aperiam doloribus. Odit, aut.
+        </span>
+      </Popup>
+      <Popup
+        trigger={<Button> Trigger </Button>}
+        position="top center"
+        closeOnDocumentClick
+      >
+        <span>
+          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
+          magni omnis delectus nemo, maxime molestiae dolorem numquam
+          mollitia, voluptate ea, accusamus excepturi deleniti ratione
+          sapiente! Laudantium, aperiam doloribus. Odit, aut.
+        </span>
+        <Rating/>
+      </Popup>
+    </Actions>
+    </StyleModal>
+    );
---
+++ client/src/components/Review/Tabs.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+import DetailsPanel from './DetailsPanel';
+import AllReviewsPanel from './AllReviewsPanel';
+import ReviewPanel from './ReviewPanel';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+ 
+export default () => (
+  <Tabs>
+    <TabList>
+      <Tab >Details</Tab>
+      <Tab>All Reviews</Tab>
+      <Tab>Leave A Review</Tab>
+    </TabList>
+ 
+    <TabPanel>
+      <DetailsPanel/>
+    </TabPanel>
+    <TabPanel>
+      <AllReviewsPanel/>
+    </TabPanel>
+    <TabPanel>
+      <ReviewPanel/>
+    </TabPanel>
+  </Tabs>
+);
