f9645a5c9f05c18ca66276831b7ff592675f03fd
Diamond Purvis <purvisdiamond@gmail.com>
2019-10-09 21:53:49 -0400

    Basic add review -not yet specific -not fully complete -added submit button -added comment area -added selection

--- client/src/components/Review/ReviewPanel.js
+++ client/src/components/Review/ReviewPanel.js
@@ -1,96 +1,154 @@
-import React from "react";
-import Popup from "reactjs-popup";
-import styled from "styled-components";
-import Rating from './Rating';
-import Modal, { ModalProvider, BaseModalBackground } from "styled-react-modal";
+import React, { Component } from 'react';
+import TextArea from "../Review/TextArea";
+import Select from "../Review/Select";
+import Button from "../Review/Button";
 
-
-const Button = styled.button`
-  /* Adapt the colors based on primary prop */
-  background: ${props => props.primary ? "blue" : "white"};
-  color: ${props => props.primary ? "white" : "blue"};
-
-  font-size: 1em;
-  margin: 1em;
-  padding: 0.25em 1em;
-  border: 2px solid blue;
-  border-radius: 3px;
-`
-
-const StyleModal = styled.div`
-    font-size: 12px;
-  `
-
-  const Header = styled.div`
-    width: 100%;
-    border-bottom: 1px solid gray;
-    font-size: 18px;
-    text-align: center;
-    padding: 5px;
- `
-  const Content = styled.div`
+  const buttonStyle = {
+    margin: "10px 10px 10px 10px"
+  };
   
-    width: 100%;
-    padding: 10px 5px;
-  `
-  const Actions = styled.div`
-    width: 100%;
-    padding: 10px 5px;
-    margin: auto;
-    text-align: center;
-  `
-  const Close = styled.div`
-    cursor: pointer;
-    position: absolute;
-    display: block;
-    padding: 2px 5px;
-    line-height: 20px;
-    right: -10px;
-    top: -10px;
-    font-size: 24px;
-    background: #ffffff;
-    border-radius: 18px;
-    border: 1px solid #cfcece;
-  `
+  
+  class ReviewPanel extends Component {
+    constructor(props) {
+      super(props);
+  
+      this.state = {
+        newUser: {
+          user_id: '',
+          rating: '',
+          internet_rating: '',
+          comments: ''
+        },
+        user_id: ["8","4","2"],
+        rating: ["1", "2", "3"],
+        internet_rating: ["1", "2", "3"],
+      };
+      
+      this.handleTextArea = this.handleTextArea.bind(this);
+      this.handleFormSubmit = this.handleFormSubmit.bind(this);
+      this.handleClearForm = this.handleClearForm.bind(this);
+      this.handleInput = this.handleInput.bind(this);
+    }
+  
+  
+  
+    handleInput(e) {
+      let value = e.target.value;
+      let name = e.target.name;
+      this.setState(
+        prevState => ({
+          newUser: {
+            ...prevState.newUser,
+            [name]: value
+          }
+        }),
+        () => console.log(this.state.newUser)
+      );
+    }
+  
+    handleTextArea(e) {
+      console.log("Inside handleTextArea");
+      let value = e.target.value;
+      this.setState(
+        prevState => ({
+          newUser: {
+            ...prevState.newUser,
+            comments: value
+          }
+        }),
+        () => console.log(this.state.newUser)
+      );
+    }
+  
+  
+  
+    handleFormSubmit(e) {
+      e.preventDefault();
+      let userData = this.state.newUser;
+  
+      fetch("https://wheretocode-master.herokuapp.com/reviews", {
+        method: "POST",
+        body: JSON.stringify(userData),
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json"
+        }
+      }).then(response => {
+        response.json().then(data => {
+          console.log("Successful" + data);
+        });
+      });
+    }
+  
+    handleClearForm(e) {
+      e.preventDefault();
+      this.setState({
+        newUser: {
+          user_id: '',
+          rating: '',
+          comments: '',
+          internet_rating: ''
+        }
+      });
+    }
+  
+    render() {
+      return (
+        <form onSubmit={this.handleFormSubmit}>
+          {/*User */}
+          <Select
+            title={"User Id"}
+            name={'user_id'}
+            options={this.state.user_id}
+            value={this.state.newUser.user_id}
+            placeholder={"Select User Id"}
+            handleChange={this.handleInput}
+          />
+          {/* Rating */}
+          <Select
+            title={"Rating"}
+            name={'rating'}
+            options={this.state.rating}
+            value={this.state.newUser.rating}
+            placeholder={"Select Rating"}
+            handleChange={this.handleInput}
+          />
+          {/*Internet Rating */}
+          <Select
+            title={"Interet Rating"}
+            name={'internet_rating'}
+            options={this.state.internet_rating}
+            value={this.state.newUser.internet_rating}
+            placeholder={"Select Internet Rating"}
+            handleChange={this.handleInput}
+          />
+          {/*Comment */}
+          <TextArea
+            title={"Comments"}
+            rows={10}
+            value={this.state.newUser.comments}
+            name={'comment'}
+            handleChange={this.handleTextArea}
+            placeholder={"Leave a comment"}
+          />
+          {/*Submit */}
+          <Button
+            action={this.handleFormSubmit}
+            type={"primary"}
+            title={"Submit"}
+            style={buttonStyle}
+          />
+          {/* Clear form */}
+          <Button
+            action={this.handleClearForm}
+            type={"secondary"}
+            title={"Clear"}
+            style={buttonStyle}
+          />
+        </form>
+      );
+    }
+  }
 
-export default ({ close }) => (
-    <StyleModal>
-    <Header> Leave A Review </Header>
-    <Content>      {" "}
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque, a nostrum.
-      Dolorem, repellat quidem ut, minima sint vel eveniet quibusdam voluptates
-      delectus doloremque, explicabo tempore dicta adipisci fugit amet dignissimos?
-      <br />
-      Lorem ipsum dolor sit amet, consectetur adipisicing elit. Consequatur sit
-      commodi beatae optio voluptatum sed eius cumque, delectus saepe repudiandae
-      explicabo nemo nam libero ad, doloribus, voluptas rem alias. Vitae?
-      </Content>    
-      <Actions>
-      <Popup
-        trigger={<Button> Trigger </Button>}
-        position="top center"
-        closeOnDocumentClick
-      >
-        <span>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-          magni omnis delectus nemo, maxime molestiae dolorem numquam
-          mollitia, voluptate ea, accusamus excepturi deleniti ratione
-          sapiente! Laudantium, aperiam doloribus. Odit, aut.
-        </span>
-      </Popup>
-      <Popup
-        trigger={<Button> Trigger </Button>}
-        position="top center"
-        closeOnDocumentClick
-      >
-        <span>
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae
-          magni omnis delectus nemo, maxime molestiae dolorem numquam
-          mollitia, voluptate ea, accusamus excepturi deleniti ratione
-          sapiente! Laudantium, aperiam doloribus. Odit, aut.
-        </span>
-        <Rating/>
-      </Popup>
-    </Actions>
-    </StyleModal>
-    );
+  export default ReviewPanel;
+  
---
+++ client/src/components/Review/Select.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Select = props => {
+  return (
+    <div>
+      <label for={props.name}> {props.title} </label>
+      <select
+        id={props.name}
+        name={props.name}
+        value={props.value}
+        onChange={props.handleChange}
+      >
+        <option value="" disabled>
+          {props.placeholder}
+        </option>
+        {props.options.map(option => {
+          return (
+            <option key={option} value={option} label={option}>
+              {option}
+            </option>
+          );
+        })}
+      </select>
+    </div>
+  );
+};
+
+export default Select;
