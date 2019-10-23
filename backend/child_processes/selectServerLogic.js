43df2aec00e0649fa1cb53d9aa25e0848c558e26
Trip1701 <bj2336@email.vccs.edu>
2019-10-22 22:21:19 -0400

    Server Filtering Complete Test 1

--- backend/child_processes/selectServerLogic.js
+++ backend/child_processes/selectServerLogic.js
@@ -6,17 +6,15 @@ const selectServer = userLocation => {
     const servers = serverList.servers.server
     
     const stateAbbr = convertState(userLocation, "abbr");
-    console.log("STATE:::::", stateAbbr);
 
     let stateServers;
-
-
+  
     if(userLocation.length > 0) {
       //filter servers based on user location
       stateServers = servers.filter(server => {
         const serverLocation = server['-name'].split(', ')[1];
 
-        return serverLocation ? serverLocation.toLowerCase() === stateAbbr
+        return serverLocation ? serverLocation.toLowerCase() === stateAbbr.toLowerCase()
                               : false;
       });
 
@@ -30,13 +28,10 @@ const selectServer = userLocation => {
       });
     }
   
-  
     //Pick and Select Random Server from List of Options
     const serverIndex = Math.round(Math.random() * stateServers.length - 1);
-  
     const selectedServer = stateServers[serverIndex];
-  
-    console.log(selectedServer, ':', selectedServer['-id']);
+
   
     return `${selectedServer['-id']}`;
   } 
