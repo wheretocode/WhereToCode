eb8f152c2b6c1c64ae3587985e3d035b8bf8d9ae
David <digital0david@gmail.com>
2019-11-05 22:28:51 -0500

    added FilteredMapCards component

---
+++ client/src/components/Map/FilteredMapCards.jsx
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import SingleMapCard from "./SingleMapCard";
+import styled from "styled-components";
+
+class FilteredMapCards extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+  }
+
+  render() {
+    return (
+      <MapCardsContainer>
+        {this.props.locationsFilter.map(location => {
+          return (
+            <div>
+              <SingleMapCard
+                key={location.id}
+                location={location.name}
+                address={location.address}
+                rating={location.rating}
+                icon={location.icon}
+                id={location.id}
+                requestDetails={this.props.requestDetails}
+              />
+            </div>
+          );
+        })}
+      </MapCardsContainer>
+    );
+  }
+}
+
+export default FilteredMapCards;
+
+const MapCardsContainer = styled.div`
+  display: flex;
+  flex-wrap: wrap;
+  width: 100%;
+  height: 100%;
+  justify-content: center;
+  overflow: scroll;
+  overflow-x: hidden;
+  &::-webkit-scrollbar {
+    display: none;
+  }
+`;
