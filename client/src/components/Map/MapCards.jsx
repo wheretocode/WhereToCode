62c92743b9b67a987aaaff8af8cf4092d32a259d
David <digital0david@gmail.com>
2019-11-06 23:46:21 -0500

    updated height %

--- client/src/components/Map/MapCards.jsx
+++ client/src/components/Map/MapCards.jsx
@@ -39,7 +39,7 @@ const MapCardsContainer = styled.div`
   display: flex;
   flex-wrap: wrap;
   width: 100%;
-  height: 86%;
+  height: 100%;
   justify-content: center;
   overflow: scroll;
   margin-right: -50px;
